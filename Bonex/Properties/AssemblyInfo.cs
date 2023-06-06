05f162aa7fe52f4a5729e49dc850690cb0261196
Millzy <millzyyt08@gmail.com>
2023-06-06 23:35:20 +0100

    assembly info

---
+++ Bonex/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+using Bonex;
+using MelonLoader;
+using BuildInfo = Bonex.BuildInfo;
+
+[assembly: AssemblyTitle(BuildInfo.name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(null!)]
+[assembly: AssemblyProduct(BuildInfo.name)]
+[assembly: AssemblyCopyright("Copyright (c) 2023 Frederick (Millzy) Mills")]
+[assembly: AssemblyTrademark(null)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion(BuildInfo.version)]
+[assembly: AssemblyFileVersion(BuildInfo.semVersion)]
+[assembly: NeutralResourcesLanguage("en")]
+
+[assembly: MelonInfo(typeof(Mod), BuildInfo.name, BuildInfo.semVersion, BuildInfo.author, 
+    BuildInfo.downloadLink)]
+
+[assembly: MelonPriority(-1000000)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonID(BuildInfo.id)]
+
+[assembly: VerifyLoaderVersion(BuildInfo.melonLoaderVersion)]
+
+[assembly: HarmonyDontPatchAll]
