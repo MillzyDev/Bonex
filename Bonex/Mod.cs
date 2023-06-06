ba20565ab97917b4e77dd5f8b569a4143cba128f
Millzy <millzyyt08@gmail.com>
2023-06-06 23:35:30 +0100

    dummy module (for now)

---
+++ Bonex/Mod.cs
@@ -0,0 +1,16 @@
+﻿using Boneject.MelonLoader;
+using Boneject.MelonLoader.Attributes;
+using Boneject.Ninject;
+using Bonex.Modules;
+
+namespace Bonex;
+
+public class Mod : InjectableMelonMod
+{
+    [OnInitialize]
+    // ReSharper disable once UnusedMember.Global
+    public void OnModInitialize(Bonejector bonejector)
+    {
+        bonejector.Load<BNXMenuModule>(Context.Menu);
+    }
+}
---
+++ Bonex/Modules/BNXMenuModule.cs
@@ -0,0 +1,12 @@
+﻿using MelonLoader;
+using Ninject.Modules;
+
+namespace Bonex.Modules;
+
+public class BNXMenuModule : NinjectModule
+{
+    public override void Load()
+    {
+        MelonLogger.Msg("Loaded");
+    }
+}
