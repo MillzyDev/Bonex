88be180f25f827788d3cc83999a42302fc543576
Millzy <millzyyt08@gmail.com>
2023-06-24 17:02:46 +0100

    for future use

---
+++ Bonex/Attributes/ViewDefinitionAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bonex.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false)]
+    public sealed class ViewDefinitionAttribute : Attribute
+    {
+        public ViewDefinitionAttribute(string definition)
+        {
+            Definition = definition;
+        }
+        
+        public string Definition { get; private set; }
+    }
+}
