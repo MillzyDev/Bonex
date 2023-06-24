33d0c38b78d5aed3334a9de00da9e4f7415e7be6
Millzy <millzyyt08@gmail.com>
2023-06-24 17:01:11 +0100

    fix assembly info

--- Bonex/Properties/AssemblyInfo.cs
+++ Bonex/Properties/AssemblyInfo.cs
@@ -1,35 +1,29 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.InteropServices;
+using Bonex;
+using MelonLoader;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Bonex")]
+[assembly: AssemblyTitle(nameof(Bonex))]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Bonex")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany(null!)]
+[assembly: AssemblyProduct(nameof(Bonex))]
+[assembly: AssemblyCopyright("Copyright (c) 2023 Frederick (Millzy) Mills")]
+[assembly: AssemblyTrademark(null)]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: NeutralResourcesLanguage("en")]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("F1106DF2-18D2-4DA2-93C1-E78EECB353CB")]
+[assembly: MelonInfo(typeof(Mod), nameof(Bonex), "0.1.0", "Millzy",
+    "https://github.com/MillzyDev/Bonex/releases/latest/download/Bonex.zip")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: MelonPriority(-1000000)]
+[assembly: MelonGame("Stress Level Zero", "BONELAB")]
+[assembly: MelonID("dev.millzy.Bonex")]
+
+[assembly: VerifyLoaderVersion("0.5.7")]
+
+[assembly: HarmonyDontPatchAll]
